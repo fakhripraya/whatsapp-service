144362beccf6e67b2f04afccbe701d38c7e73ced
unknown <fakhripraya.p@gmail.com>
2021-01-03 14:02:25 +0700

    update handling error whatsapp client invalid websocket

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/fakhripraya/whatsapp-service/data"
 	"github.com/fakhripraya/whatsapp-service/entities"
@@ -61,14 +63,49 @@ func main() {
 	reflection.Register(gs)
 
 	// create a TCP socket for inbound server connections
-	logger.Info("Creating TCP socket on " + appConfig.AppConfig.Host + ":" + appConfig.AppConfig.Port)
-	listener, err := net.Listen("tcp", fmt.Sprintf(":"+appConfig.AppConfig.Port))
-	if err != nil {
-		logger.Error("Unable to create listener", "error", err.Error())
-		os.Exit(1)
-	}
+	// start the server
+	go func() {
+		logger.Info("Creating TCP socket on " + appConfig.AppConfig.Host + ":" + appConfig.AppConfig.Port)
+		listener, err := net.Listen("tcp", fmt.Sprintf(":"+appConfig.AppConfig.Port))
+		if err != nil {
+			logger.Error("Unable to create listener", "error", err.Error())
+			os.Exit(1)
+		}
 
-	// listen for requests
-	logger.Info("Successfully creating TCP socket")
-	gs.Serve(listener)
+		// listen for requests
+		logger.Info("Successfully creating TCP socket")
+		gs.Serve(listener)
+	}()
+
+	go func() {
+		for {
+			if waConfig.Wac.GetConnected() == false {
+
+				// gracefully stop all incoming gRPCs request within 30 seconds
+				gs.GracefulStop()
+				time.Sleep(30 * time.Second)
+
+				// exit with 0 status code, so the server can restart
+				os.Exit(0)
+			}
+		}
+	}()
+
+	// trap sigterm or interrupt and gracefully shutdown the server
+	channel := make(chan os.Signal, 1)
+	signal.Notify(channel, os.Interrupt)
+	signal.Notify(channel, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-channel
+	logger.Info("Got signal", "info", sig)
+
+	logger.Info("Gracefully stopping the gRPC server")
+
+	// gracefully stop all incoming gRPCs request within 30 seconds
+	gs.GracefulStop()
+	time.Sleep(30 * time.Second)
+
+	// exit with 0 status code, so the server can restart
+	os.Exit(0)
 }
